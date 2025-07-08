e9f687f2cb413983d94521ddbfa0f89c0fa9d2b2
Hirusha Dayarathna <geekhirusha@gmail.com>
2025-07-08 12:21:56 +0530

    Refactor project structure and add MySQL support
    Renamed project directory to 'WEB II PROJECT 05 (Backend)' and updated all related paths. Added MySQL Connector/J dependency and updated build files for library inclusion. Replaced and enhanced CreateNewAccount servlet to handle user creation with database integration. Simplified User model by removing redundant comments and constructors.

---
+++ WEB II PROJECT 05 (Backend)/src/java/controller/CreateNewAccount.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+@WebServlet(name = "CreateNewAccount", urlPatterns = {"/CreateNewAccount"})
+public class CreateNewAccount extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+
+//        Json -> Java
+        Gson gson = new Gson();
+        User user = gson.fromJson(request.getReader(), User.class);
+
+        // System.out.println(user.getMobile());
+        // System.out.println(user.getFirstname());
+        // System.out.println(user.getLastname());
+        // System.out.println(user.getPassword());
+        // System.out.println(user.getCountry());
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection c = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/web_5?useSSL=false", "root", "password");
+            Statement s = c.createStatement();
+            s.executeUpdate("INSERT INTO users (mobile, firstname, lastname, password, country) VALUES ('" + user.getMobile() + "', '" + user.getFirstname() + "', '" + user.getLastname() + "', '" + user.getPassword() + "', '" + user.getCountry() + "')");
+            response.getWriter().write("Success");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write("Error: " + e.getMessage());
+        }
+
+        response.getWriter().write("Hello!");
+
+    }
+}
--- WEB II PROJECT 05 (Backend)/src/java/model/User.java
+++ WEB II PROJECT 05 (Backend)/src/java/model/User.java
@@ -12,75 +12,48 @@ public class User {
     private String password;
     private String country;
 
-    /**
-     * @return the mobile
-     */
+    public User() {
+
+    }
+
     public String getMobile() {
         return mobile;
     }
 
-    /**
-     * @param mobile the mobile to set
-     */
     public void setMobile(String mobile) {
         this.mobile = mobile;
     }
 
-    /**
-     * @return the firstname
-     */
     public String getFirstname() {
         return firstname;
     }
 
-    /**
-     * @param firstname the firstname to set
-     */
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
-    /**
-     * @return the lastname
-     */
     public String getLastname() {
         return lastname;
     }
 
-    /**
-     * @param lastname the lastname to set
-     */
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    /**
-     * @return the password
-     */
     public String getPassword() {
         return password;
     }
 
-    /**
-     * @param password the password to set
-     */
     public void setPassword(String password) {
         this.password = password;
     }
 
-    /**
-     * @return the country
-     */
     public String getCountry() {
         return country;
     }
 
-    /**
-     * @param country the country to set
-     */
     public void setCountry(String country) {
         this.country = country;
     }
 
-    
 }
