8f6cca7d020f9d155153050315f2781a7023e6d4
Hirusha Dayarathna <geekhirusha@gmail.com>
2025-07-08 11:32:34 +0530

    Add AJAX request to createNewAccount function
    Refactored createNewAccount to collect form data into a JSON object and send it to the server using an XMLHttpRequest POST call. Console logging of individual fields was replaced with logging the JSON string, and error handling for the AJAX response was added.

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 function createNewAccount() {
-    const mobile = document.getElementById("mobile").value;
-    const firstname = document.getElementById("firstname").value;
-    const lastname = document.getElementById("lastname").value;
-    const password = document.getElementById("password").value;
-    const country = document.getElementById("country").value;
+  // Get the values from the input fields
+  const mobile = document.getElementById("mobile").value;
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const password = document.getElementById("password").value;
+  const country = document.getElementById("country").value;
 
-    // Validate the inputs
-    console.log(mobile);
-    console.log(firstname);
-    console.log(lastname);
-    console.log(password);
-    console.log(country);
+  // Validate the inputs
+  // console.log(mobile);
+  // console.log(firstname);
+  // console.log(lastname);
+  // console.log(password);
+  // console.log(country);
 
+  // JS Object to hold the data
+  const userData = {
+    mobile: mobile,
+    firstname: firstname,
+    lastname: lastname,
+    password: password,
+    country: country,
+  };
+
+  // Convert the object to a JSON string
+  const userDataJSON = JSON.stringify(userData);
+
+  // Log the JSON string to the console
+  console.log(userDataJSON);
+
+  // add ajax call here to send the data to the server
+  const ajax = new XMLHttpRequest();
+  ajax.open("POST", "http://localhost:8080/WEB_II_PROJECT_05/CreateNewAccount", true);
+
+  ajax.onreadystatechange = function () {
+    if (ajax.readyState === 4) {
+      if (ajax.status === 200) {
+        console.log("Account created successfully:", ajax.responseText);
+      } else {
+        console.error("Error creating account:", ajax.status, ajax.statusText);
+      }
+    }
+  };
+
+  ajax.send();
 }
