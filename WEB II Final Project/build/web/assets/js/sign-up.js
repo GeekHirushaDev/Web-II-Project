3356c6e0164a9af03c640b4e1a7d97dc2a127245
Hirusha Dayarathna <geekhirusha@gmail.com>
2025-07-15 10:41:44 +0530

    Add input IDs and enhance sign-up logging
    Added unique IDs to sign-up form fields for first name, last name, email, and password. Updated signUp() function to log user input values for improved debugging and data handling.

--- WEB II Final Project/build/web/assets/js/sign-up.js
+++ WEB II Final Project/build/web/assets/js/sign-up.js
@@ -1,3 +1,9 @@
 function signUp(){
-    console.log("Ok")
+    
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    
+    console.log(firstName+" "+lastName+"'s email is " +email+" and password is "+password);
 }
