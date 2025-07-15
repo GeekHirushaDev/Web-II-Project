7627d4f618923e232124d2a04c9b646497213201
Hirusha Dayarathna <geekhirusha@gmail.com>
2025-07-15 10:58:41 +0530

    Refactor signUp to use jQuery and create user object
    Replaced vanilla JS with jQuery for firstName input retrieval. Added user object construction and JSON serialization for sign-up data. Commented out console logging for cleaner code.

--- WEB II Final Project/web/assets/js/sign-up.js
+++ WEB II Final Project/web/assets/js/sign-up.js
@@ -1,9 +1,21 @@
 function signUp(){
     
-    const firstName = document.getElementById("firstName").value;
+    const firstName = $("#firstName").val(); // using jQuery
     const lastName = document.getElementById("lastName").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     
-    console.log(firstName+" "+lastName+"'s email is " +email+" and password is "+password);
+    // console.log(firstName+" "+lastName+"'s email is " +email+" and password is "+password);
+
+    const user = {
+        firstName:firstName,
+        lastName:lastName,
+        email:email,
+        password:password
+    };
+    
+    const userJson = JSON.stringify(user);
+    
+    
+
 }
